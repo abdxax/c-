39bb2df4e4897a528e393f6124ffc27fa05d7b5a
unknown <abdxax@gmail.com>
2019-12-02 21:43:58 +0300

    s

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -4,73 +4,110 @@ namespace ConsoleApp9
 {
     class Program
     {
+        static String[] drug = new String[] { "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10" };
+        static int[] quan = new int[] { 2, 3, 4, 8, 5, 6, 7, 8, 9, 10 };
+        static double[] price = new double[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+        static void disolayStok()
+        {
+            for (int i = 0; i < drug.Length; i++)
+            {
+                Console.WriteLine(drug[i] + " " + quan[i]);
+            }
+
+        }
+
+        static Boolean enterOrder()
+        {
+            Console.WriteLine("Please Enter the name drgugs");
+            String named = Console.ReadLine();
+            Console.WriteLine("Please Enter the quantity");
+            int q = int.Parse(Console.ReadLine());
+            Boolean isFound = false;
+
+            for (int i = 0; i < drug.Length; i++)
+            {
+                if (named == drug[i])
+                {
+                    isFound = true;
+                }
+                else
+                {
+
+                    //break;
+                }
+                if (isFound)
+
+                {
+                    if (quan[i] >= q)
+                    {
+                        int qu = quan[i] - q;
+                        quan[i] = qu;
+                        Console.WriteLine("The drug is " + named + " The quantit " + q + " The price is " + (price[i] * q));
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Quantity availble in stoke is " + quan[i]);
+                        return true;
+                    }
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("The drug not availible now try again ");
+            }
+            return false;
+
+
+        }
+
+
+
+
 
         static void Main(string[] args)
         {
             //  String drugs[] = new string[20];
-            String[] drug = new String[] { "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10" };
-            int[] quan = new int[] { 2, 3, 4, 8, 5, 6, 7, 8, 9, 10 };
-            double[] price = new double[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
-            Console.WriteLine("Please Choise from option \n 1- stoke detels \n 2- enter order ");
-            int op = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello World!");
 
-            switch (op)
+            Boolean wh = true;
+
+            while (wh)
             {
-                case 1:
-                    for (int i = 0; i < drug.Length; i++)
-                    {
-                        Console.WriteLine(drug[i] + " " + quan[i]);
-                    }
-                    break;
+                Console.WriteLine("Please Choise from option \n 1- stoke detels \n 2- enter order  \n 3-exeit the system ");
+                int op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Program.disolayStok();
+                        break;
 
-                case 2:
-                    Console.WriteLine("Please Enter the name drgugs");
-                    String named = Console.ReadLine();
-                    Console.WriteLine("Please Enter the quantity");
-                    int q = int.Parse(Console.ReadLine());
-                    Boolean isFound = false;
-
-                    for (int i = 0; i < drug.Length; i++)
-                    {
-                        if (named == drug[i])
+                    case 2:
+                        Boolean o = Program.enterOrder();
+                        if (o)
                         {
-                            isFound = true;
+                            Console.WriteLine("Order Complite ");
                         }
-                        else
-                        {
 
-                            //break;
-                        }
-                        if (isFound)
+                        break;
 
-                        {
-                            if (quan[i] >= q)
-                            {
-                                int qu = quan[i] - q;
-                                quan[i] = qu;
-                                Console.WriteLine("The drug is " + named + " The quantit " + q + " The price is " + (price[i] * q));
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("The Quantity availble in stoke is " + quan[i]);
-                                break;
-                            }
-                        }
-                    }
-                    if (!isFound)
-                    {
-                        Console.WriteLine("The drug not availible now try again ");
-                    }
-                    Console.ReadKey();
+                    case 3:
 
-                    break;
+                        System.Environment.Exit(1);
+                        break;
+
+
+
+                }
+
+                // Console.WriteLine("Hello World!");
+
             }
-
-            // Console.WriteLine("Hello World!");
-
+            Console.ReadKey();
         }
     }
 }
+
 
